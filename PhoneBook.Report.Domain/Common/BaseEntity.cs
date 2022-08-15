8e80b07c796db59f24cbb6479369f253fc20c1c3
Azize Aydoğdu Kara <aydogduazize@gmail.com>
2022-08-15 11:10:46 +0300

    Report api updated.

--- PhoneBook.Report.Domain/Common/BaseEntity.cs
+++ PhoneBook.Report.Domain/Common/BaseEntity.cs
@@ -1,9 +1,12 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace PhoneBook.Report.Domain.Common;
 
 public abstract class BaseEntity
 {
-    // [BsonId]
-    // [BsonRepresentation(BsonType.ObjectId)]
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
     public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
     public bool IsDeleted { get; set; } = false;
